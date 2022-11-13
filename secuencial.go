227fe05eaea5267711a9ae5225cbc31d3cdd3a02
frankguido <38305115+frankguido@users.noreply.github.com>
2022-11-13 12:12:13 -0500

    Update secuencial.go

--- secuencial.go
+++ secuencial.go
@@ -8,15 +8,14 @@ import (
 // funcion main
 func main() {
 	//creando dos procesos para la funcion main
-	name("Felix")
 	name("Nat")
+	name("Felix")
 
 }
 
 // funcion process para imprimir n veces cada nombre
 func name(item string) {
 	for i := 1; i <= 10; i++ {
-
 		fmt.Println(item, i)
 	}
 }
